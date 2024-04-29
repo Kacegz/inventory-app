3b0ab4df49d282b737b6cf972e23b064bbea797e
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-04-29 21:37:16 +0200

    setup mock controllers

--- controllers/teaController.js
+++ controllers/teaController.js
@@ -1,5 +1,30 @@
 const Tea = require("../models/tea");
-
 const asyncHandler = require("express-async-handler");
 
-exports.index = asyncHandler(async (res, req) => {});
+exports.index = asyncHandler(async (req, res) => {
+  res.send("Not implemented yet");
+});
+exports.tea_list = asyncHandler(async (req, res) => {
+  res.send("Not implemented: tea list");
+});
+exports.tea_detail = asyncHandler(async (req, res) => {
+  res.send(`not implemented: tea detail ${req.params.id}`);
+});
+exports.tea_create_get = asyncHandler(async (req, res) => {
+  res.send("not impelemented: tea create get");
+});
+exports.tea_create_post = asyncHandler(async (req, res) => {
+  res.send("not impelemented: tea create post");
+});
+exports.tea_delete_get = asyncHandler(async (req, res) => {
+  res.send("not impelemented: tea delete get");
+});
+exports.tea_delete_post = asyncHandler(async (req, res) => {
+  res.send("not impelemented: tea delete post");
+});
+exports.tea_update_get = asyncHandler(async (req, res) => {
+  res.send("not impelemented: tea create get");
+});
+exports.tea_update_post = asyncHandler(async (req, res) => {
+  res.send("not impelemented: tea update post");
+});
--- controllers/teatypeController.js
+++ controllers/teatypeController.js
@@ -1,11 +1,31 @@
 const teaType = require("../models/teaType");
-
 const asyncHandler = require("express-async-handler");
 
-exports.teatype_list = asyncHandler(async (res, req) => {
-  const alltypes = await teaType.find().exec();
+exports.teatype_list = asyncHandler(async (req, res) => {
+  const alltypes = await teaType.find().sort({ name: 1 }).exec();
   res.render("teatype_list", {
     title: "Tea type list:",
     teatype_list: alltypes,
   });
+});
+exports.teatype_detail = asyncHandler(async (req, res) => {
+  res.send(`not implemented: tea type detail ${req.params.id}`);
+});
+exports.teatype_create_get = asyncHandler(async (req, res) => {
+  res.send("not implemented: tea type create get");
+});
+exports.teatype_create_post = asyncHandler(async (req, res) => {
+  res.send("not implemented: tea type create post");
+});
+exports.teatype_delete_get = asyncHandler(async (req, res) => {
+  res.send("not implemented: tea type delete get");
+});
+exports.teatype_delete_post = asyncHandler(async (req, res) => {
+  res.send("not implemented: tea type delete post");
+});
+exports.teatype_update_get = asyncHandler(async (req, res) => {
+  res.send("not implemented: tea type update get");
+});
+exports.teatype_update_post = asyncHandler(async (req, res) => {
+  res.send("not implemented: tea type update post");
 });
