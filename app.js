1bc4d5008fe58ea2723dd0bf3a430c17287fe164
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-05-03 17:09:59 +0200

    Add compression and helmet

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 require("dotenv").config();
+const compression = require("compression");
+const helmet = require("helmet");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -22,6 +24,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(compression());
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
